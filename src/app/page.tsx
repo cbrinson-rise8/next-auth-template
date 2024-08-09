71dfd84e279aa4f7a48cc1534a328667c0b40fe6
cason <cbrinson@rise8.us>
2024-08-09 12:47:10 -0400

    added register button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-
   const router = useRouter();
 
   return (
-    <main className="flex flex-col min-h-screen justify-between items-center p-24">
+    <main className="relative flex flex-col min-h-screen justify-between items-center p-24">
       <h1 className="font-bold text-lg">Public Facing Page</h1>
-      <button onClick={() => router.push('/login')} className="p-4 rounded-full bg-green-200 border-2 border-black">Click To Login</button>
+      <div className="flex flex-col space-y-4">
+        <button
+          onClick={() => router.push("/login")}
+          className="p-4 rounded-full bg-green-200 border-2 border-black"
+        >
+          Login
+        </button>
+        <button
+          onClick={() => router.push("/register")}
+          className="p-4 rounded-full bg-green-200 border-2 border-black"
+        >
+          Register
+        </button>
+      </div>
     </main>
   );
 }
